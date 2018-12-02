77a328e6ebf268a6c9a27d142923dd59924ab8de
Jose <jbenitez@conwet.com>
2018-12-02 22:10:51 +0100

    arrancar en estado de producción o test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Utilizar funcionalidades del Ecmascript 6
-'use strict';
+'use strict'
 
 // Cargamos el módulo de mongoose para poder conectarnos a MongoDB
 var mongoose = require('mongoose');
@@ -8,25 +8,28 @@ var mongoose = require('mongoose');
 var app = require('./app');
 
 // Creamos la variable PORT para indicar el puerto en el que va a funcionar el servidor
-var config = require('./config.json');
+const config= require('./config.json');
 
 //Tomamos los valores del fichero de configuración
-var conf = config.development;
+var conf=config.development;
 var node_port = conf.node_port;
-var mongo_conection = `${conf.direction}:${conf.mongo_port}/${conf.database}`;
-var listen = `${conf.direction}:${node_port}`;
+var mongo_conection=`:${conf.mongo_port}/${conf.database}`;
+var listen= `${conf.direction}:${node_port}`;
 // Le indicamos a Mongoose que haremos la conexión con Promesas
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(`mongodb://${mongo_conection}`, { useNewUrlParser: true })
 
 // Usamos el método connect para conectarnos a nuestra base de datos
-mongoose.connect(`mongodb://mongo:${conf.mongo_port}/${conf.database}`,  { useNewUrlParser: true })
+if(process.env.NODE_ENV=="production"){
+    mongo_conection= conf.directionProduction + mongo_conection;
+}else{
+    mongo_conection= conf.direction + mongo_conection;
+}
+mongoose.connect(`mongodb://${mongo_conection}`, { useNewUrlParser: true})
     .then(() => {
-
         // Cuando se realiza la conexión, lanzamos este mensaje por consola
         console.log("La conexión a la base de datos dashboards se ha realizado correctamente")
-
+    
         // CREAR EL SERVIDOR WEB CON NODEJS
         app.listen(node_port, () => {
             console.log(`servidor corriendo en http://${listen}`);
