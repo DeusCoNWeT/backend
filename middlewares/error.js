d054b2db97e44693a7bea4aaa0a6f0cd3214f974
Jose <jbenitez@conwet.com>
2018-12-25 22:08:58 +0100

    eliminamos middleware innecesario

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,9 +1,7 @@
 'user strict';
 
-
-var status = require('../middlewares/statusCodes.js')
 module.exports = function(err,req,res,next){
-    //logger.logger.error(`Error interno del sistema ${err}`);
-    //res.status(500).send('Error interno del sistema');
-    status.codes("500", res, null, err, null)
+    logger.logger.error(`Se ha producido un error interno en el sistema | ${err}`);
+    res.status(500).send({ message: 'Error interno del sistema' });
+            
 }
